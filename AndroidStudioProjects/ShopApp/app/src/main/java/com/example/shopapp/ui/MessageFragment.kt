6d64f0a78b14e247cf6ebe7b8e70da8d1c72eac9
Aleksandra <akkovaleva14@gmail.com>
2024-09-12 16:41:00 +0300

    cleaned code, adjusted keyboard

--- AndroidStudioProjects/ShopApp/app/src/main/java/com/example/shopapp/ui/MessageFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.example.shopapp.ui
-
-import androidx.fragment.app.Fragment
-import com.example.shopapp.R
-
-class MessageFragment : Fragment(R.layout.fragment_message)
--- AndroidStudioProjects/ShopApp/app/src/main/java/com/example/shopapp/ui/SearchFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.example.shopapp.ui
-
-import androidx.fragment.app.Fragment
-import com.example.shopapp.R
-
-class SearchFragment : Fragment(R.layout.fragment_search)
